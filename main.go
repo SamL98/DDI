94f1acecd921da466201744c1e8ee70849015063
Sam Lerner <lerner98@gmail.com>
2017-09-04 22:00:26 -0400

    $1

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
